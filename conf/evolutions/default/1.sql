a63439cd6547cdd73edffe82554880c3c737edfc
slayer <demchuck.dima@gmail.com>
2012-07-23 22:16:45 +0300

    fixing database to fit heroku

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,29 +2,29 @@
 
 # --- !Ups
 
-CREATE TABLE `regions` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `name` TEXT NOT NULL,
-  `sort` BIGINT(20) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARSET=utf8;
+CREATE TABLE regions (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  sort BIGINT(20) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE `site_config` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `time_offset` int(11) NOT NULL default '0',
-  `site_name_ru` text NOT NULL,
-  `title_text_ru` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE site_config (
+  id bigint(20) NOT NULL auto_increment,
+  time_offset int(11) NOT NULL default '0',
+  site_name_ru text NOT NULL,
+  title_text_ru text NOT NULL,
+  PRIMARY KEY  (id)
+);
 
-CREATE TABLE `counters` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL default '',
-  `text` text NOT NULL,
-  `sort` bigint(20) unsigned NOT NULL default '0',
-  `active` char(1) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE counters (
+  id bigint(20) NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '',
+  text text NOT NULL,
+  sort bigint(20) unsigned NOT NULL default '0',
+  active char(1) NOT NULL default '',
+  PRIMARY KEY  (id)
+);
 
 # --- !Downs
 
