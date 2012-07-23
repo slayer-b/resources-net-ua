5d56f63a70f1540338cc953be956ac8c88ba9ef6
slayer <demchuck.dima@gmail.com>
2012-07-23 22:42:22 +0300

    asd

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,33 +2,32 @@
 
 # --- !Ups
 
-drop table regions;
-drop table site_config;
-drop table counters;
-
-CREATE TABLE regions (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE regions(
+  id BIGINT NOT NULL,
   name TEXT NOT NULL,
-  sort BIGINT(20) NOT NULL DEFAULT 0,
+  sort BIGINT NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE site_config (
-  id bigint(20) NOT NULL auto_increment,
+CREATE TABLE site_config(
+  id bigint(20) NOT NULL,
   time_offset int(11) NOT NULL default '0',
   site_name_ru text NOT NULL,
   title_text_ru text NOT NULL,
   PRIMARY KEY  (id)
 );
 
-CREATE TABLE counters (
-  id bigint(20) NOT NULL auto_increment,
+CREATE TABLE counters(
+  id bigint(20) NOT NULL,
   name varchar(255) NOT NULL default '',
   text text NOT NULL,
-  sort bigint(20) unsigned NOT NULL default '0',
+  sort bigint(20) NOT NULL default '0',
   active char(1) NOT NULL default '',
   PRIMARY KEY  (id)
 );
 
 # --- !Downs
 
+drop table regions;
+drop table site_config;
+drop table counters;
