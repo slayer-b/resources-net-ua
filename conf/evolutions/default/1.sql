c781094d5247efee31881076966c0f563f035966
slayer <demchuck.dima@gmail.com>
2012-07-25 00:13:04 +0300

    moving to postgres

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,14 +3,14 @@
 # --- !Ups
 
 CREATE TABLE regions(
-  id BIGINT NOT NULL,
+  id bigserial NOT NULL,
   name TEXT NOT NULL,
   sort BIGINT NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE site_config(
-  id bigint NOT NULL,
+  id bigserial NOT NULL,
   time_offset int NOT NULL default '0',
   site_name_ru text NOT NULL,
   title_text_ru text NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE site_config(
 );
 
 CREATE TABLE counters(
-  id bigint NOT NULL,
+  id bigserial NOT NULL,
   name varchar(255) NOT NULL default '',
   text text NOT NULL,
   sort bigint NOT NULL default '0',
