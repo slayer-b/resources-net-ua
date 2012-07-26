8609b82cc6609bcaa40e4989b882558f810b282f
slayer <demchuck.dima@gmail.com>
2012-07-26 23:54:30 +0300

    removed ';' from evolution script, it breaks evolutions

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -6,15 +6,15 @@ CREATE TABLE counters(
   id bigserial NOT NULL,
   name varchar(255) NOT NULL default '',
   text text NOT NULL,
-  sort bigint NOT NULL default '0',
+  sort bigint NOT NULL default 0,
   active boolean NOT NULL default false,
   PRIMARY KEY  (id)
 );
 
-insert into counters(name, text, sort, active) values('1', '<div style="color:blue;">1 - blue</div>', 4, true);
-insert into counters(name, text, sort, active) values('1', '<div style="color:red;">2 - red</div>', 3, true);
-insert into counters(name, text, sort, active) values('1', '<div style="color:green;">3 - green</div>', 2, true);
-insert into counters(name, text, sort, active) values('1', '<div style="color:purple;">4 - purple</div>', 1, true);
+insert into counters(name, text, sort, active) values('1', '<div style="color:blue">1 - blue</div>', 4, true);
+insert into counters(name, text, sort, active) values('1', '<div style="color:red">2 - red</div>', 3, true);
+insert into counters(name, text, sort, active) values('1', '<div style="color:green">3 - green</div>', 2, true);
+insert into counters(name, text, sort, active) values('1', '<div style="color:purple">4 - purple</div>', 1, true);
 
 # --- !Downs
 
